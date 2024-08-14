a53d8861576810e6ae1ad5366f501b64c0081426
sowmik-sec <sowmik.sec@gmail.com>
2024-08-14 21:37:22 +0600

    use post to load some products using keys

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +37,20 @@ async function run() {
         .skip(page * size)
         .limit(size)
         .toArray();
+      res.send(result);
+    });
+
+    app.post("/productByIds", async (req, res) => {
+      const ids = req.body;
+
+      const idsWithObjectId = ids.map((id) => new ObjectId(id));
+      console.log(idsWithObjectId);
+      const query = {
+        _id: {
+          $in: idsWithObjectId,
+        },
+      };
+      const result = await productCollection.find(query).toArray();
       res.send(result);
     });
 
